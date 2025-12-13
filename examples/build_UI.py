361e5d31c386b554aceef90a983c90d15ddc0bd9
Jaewon Cho <112532935+Jaejae1107@users.noreply.github.com>
2025-12-13 04:25:51 -0500

    Update build_UI.py

--- examples/build_UI.py
+++ examples/build_UI.py
@@ -651,7 +651,7 @@ def create_demo():
 if __name__ == "__main__":
     demo = create_demo()
     demo.launch(
-        server_name="0.0.0.0",
+        server_name="localhost",
         server_port=7860,
         share=False
     )
